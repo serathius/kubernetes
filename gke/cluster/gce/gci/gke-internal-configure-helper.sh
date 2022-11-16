139c0505e1c1b727cf08156076dc5a28a097a71c
Grace Gao <gracegao@google.com>
2022-11-16 22:06:23 +0000

    change PathModified to PathExists
    Change-Id: Ib4c7152edff206cbd481de4e60fe43439606f024

--- gke/cluster/gce/gci/gke-internal-configure-helper.sh
+++ gke/cluster/gce/gci/gke-internal-configure-helper.sh
@@ -792,7 +792,7 @@ EOF
 Description=NVIDIA MPS path
 
 [Path]
-PathModified=/home/kubernetes/bin/nvidia/bin/nvidia-cuda-mps-control
+PathExists=/home/kubernetes/bin/nvidia/bin/nvidia-cuda-mps-control
 Unit=nvidia-mps.service
 
 [Install]
