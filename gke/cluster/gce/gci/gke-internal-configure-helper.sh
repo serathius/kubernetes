c50eb44609e9fc06b820143c752509e8bf025aae
Ayush Ranjan <ayushranjan@google.com>
2023-06-13 19:11:26 +0000

    gvisor: Make GKE Sandbox use default --directfs setting.
    --directfs=true has been made the default in OSS runsc. This has soaked in
    OSS for 3+ weeks now. Roll out this change to GKE Sandbox.
    
    Change-Id: Ida5f10881ec12e77516dca9674792d36d29529d1

--- gke/cluster/gce/gci/gke-internal-configure-helper.sh
+++ gke/cluster/gce/gci/gke-internal-configure-helper.sh
@@ -769,7 +769,6 @@ EOF
     local -r gvisor_net_raw="${GVISOR_NET_RAW:-"true"}"
     local -r gvisor_seccomp="${GVISOR_SECCOMP:-"true"}"
     local -r gvisor_core_tags="${GVISOR_CORE_TAGS:-"false"}"
-    local -r gvisor_directfs="${GVISOR_DIRECTFS:-"false"}"
     mkdir -p "${sandbox_root}"
     cat > "${shim_config_path}" <<EOF
 binary_name = "${CONTAINERD_SANDBOX_RUNTIME_ENGINE:-}"
@@ -780,7 +779,6 @@ root = "${sandbox_root}"
   oci-seccomp = "${gvisor_seccomp}"
   systemd-cgroup = "${systemdCgroup}"
   enable-core-tags = "${gvisor_core_tags}"
-  directfs = "${gvisor_directfs}"
 EOF
     if [[ -n "${GVISOR_METRIC_SERVER:-}" ]]; then
       echo "  metric-server = \"${GVISOR_METRIC_SERVER}\"" >> "${shim_config_path}"
