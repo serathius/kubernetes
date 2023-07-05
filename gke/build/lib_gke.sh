2172c9a98bf936ac2bb37a20d4056215f4871750
Jordan Liggitt <liggitt@google.com>
2023-07-05 19:31:22 +0000

    gke/build: use go-provided docker base image
    Change-Id: Ib07ca93db00e5535d46a1ce16c0a9ab5885a4c64

--- gke/build/lib_gke.sh
+++ gke/build/lib_gke.sh
@@ -760,7 +760,7 @@ set_global_vars()
   if [[ -z "${__golang_image}" ]]; then
     log.info "no build-env.compiler-image.deps.golang-image specified, using .go-version if available"
     __golang_image_version=$(cat "${KUBE_ROOT}/.go-version")
-    __golang_image="golang:${__golang_image_version}-bullseye"
+    __golang_image="google-go.pkg.dev/golang:${__golang_image_version}"
     log.debugvar __golang_image
   fi
 
