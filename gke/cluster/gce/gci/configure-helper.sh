ec0f27e82f2423776e664f44db8d7e24d04387aa
Sergey Kanzhelev <skanzhelev@google.com>
2024-02-16 23:22:02 +0000

    remove the ENABLE_POD_LOG logic completely
    It is disabled for 4 years now: https://github.com/kubernetes/kubernetes/pull/93781/. Cleaning up after looking at this logic for a customer's question.
    
    Change-Id: Ife45d651a484a58442f0283a7fd4d9826f48a70c

--- gke/cluster/gce/gci/configure-helper.sh
+++ gke/cluster/gce/gci/configure-helper.sh
@@ -626,24 +626,6 @@ function setup-logrotate() {
 }
 EOF
   fi
-
-  if [[ "${ENABLE_POD_LOG:-false}" = "true" ]]; then
-    # Configure log rotation for pod logs in /var/log/pods/NAMESPACE_NAME_UID.
-    cat > /etc/logrotate.d/allpodlogs <<EOF
-/var/log/pods/*/*.log {
-    rotate ${POD_LOG_MAX_FILE:-5}
-    copytruncate
-    missingok
-    notifempty
-    compress
-    maxsize ${POD_LOG_MAX_SIZE:-5M}
-    daily
-    dateext
-    dateformat -%Y%m%d-%s
-    create 0644 root root
-}
-EOF
-  fi
 }
 
 # Finds the master PD device; returns it in MASTER_PD_DEVICE
