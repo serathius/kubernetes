08d287e489a418c8d58fce8757f36df3404c7eb8
David Porter <porterdavid@google.com>
2024-10-07 16:21:18 -0700

    Update placeholders presuspend script
    * Adjust trigger used to detect resume. Instead of relying on GCE
      metadata, as before, install a new systemd service which will run
      on GCE VM resume. This is done as metadata is not immediately reachable
      on resume. Update `wait_for_suspension` to poll if the systemd service
      ran as opposed to querying GCE metadata.
    * Make several changes on resume:
      * Reload gvnic kernel module. This is necessary as the MAC address of
        the VM will change on resume and to refresh the address, the module
        must be reloaded in the guest. See b/365605093 for additional
        details.
      * After gvnic module is reloaded; block execution until the network
        connectivity is re-established. This is done by restarting the
        systemd-networkd-wait-online.service service and gcr-wait-online.service
        services. This ensures that the bootstrap will only resume when
        network connectivity is fully re-established.
      * Restart `google-guest-agent.service` on resume. The GCE guest agent
        caches the instance ID, and as such restarting it will ensure it
        will pick up the new project and instance ID after resume. See
        http://b/359365370#comment3 for more context.
    
    BUG=359365370
    
    Change-Id: I61b854065b6b596db1e6c6c915eaf8d927310d18
    Signed-off-by: David Porter <porterdavid@google.com>

--- gke/cluster/gce/gci/placeholders-presuspend.sh
+++ gke/cluster/gce/gci/placeholders-presuspend.sh
@@ -12,46 +12,90 @@ write_to_mds() {
   /usr/bin/curl --fail --retry 5 --retry-delay 3 --silent --show-error -X PUT --data "$data" "http://metadata.google.internal/computeMetadata/v1/instance/$path" -H "Metadata-Flavor: Google"
 }
 
-get_cluster_name() {
-  echo $(/usr/bin/curl --fail --retry 5 --retry-delay 3 --silent --show-error -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/attributes/cluster-name" || true)
+log() {
+  # Get current time.
+  local current_time
+  current_time="$(date --utc '+%s.%N')"
+  # ...formatted as UTC RFC 3339.
+  local timestamp
+  timestamp="$(date --utc --date="@${current_time}" '+%FT%T.%NZ')"
+
+  msg="$1"
+  echo "placeholders-presuspend.sh ${timestamp} ${msg}"
 }
 
 wait_for_suspension() {
-  clusterName="$(get_cluster_name)"
-  echo "Waiting for cluster-name metadata to be set"
-  while [ "$clusterName" == "" ];
-  do
-    sleep 1
-    clusterName="$(get_cluster_name)"
+  log "Waiting for resume"
+  while ! systemctl is-active --quiet gke-placeholder-resume-trigger.service; do
+    sleep "0.2"
+    log "Waiting for gke-placeholder-resume-trigger..."
   done
-  echo "Fetched cluster name: $clusterName"
 }
 
+
 handle_placeholder_vm() {
-  echo "It is a Placeholder VM"
+  log "Starting placeholders-presuspend.sh"
+
+  if curl --fail --retry 5 --retry-delay 3 --silent --output /dev/null -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/attributes/cluster-name"; then
+    log "cluster-name is already set; exiting placeholders-presuspend.sh"
+    return 0
+  fi
+
+  log "Marking as initailizing in MDS..."
   write_to_mds "$INIT_STATE_GUEST_ATTRIBUTE" "INITIALIZING"
 
+  cat <<EOF >/etc/systemd/system/gke-placeholder-resume-trigger.service
+[Unit]
+Description=GKE Placeholder Resume Trigger
+After=suspend.target
+
+[Service]
+Type=oneshot
+RemainAfterExit=yes
+ExecStart=/bin/true
+
+[Install]
+WantedBy=suspend.target
+EOF
+
+  systemctl daemon-reload
+  systemctl enable gke-placeholder-resume-trigger.service
+
+
   # TODO: We hardcode the driver verision to latest temporarily.
-  # Once we have the desired GPU driver version in the suspended state group, we will read it from GCE metdata and pass it to the --version.
+  # Once we have the desired GPU driver version in the suspended state group, we will read it from GCE metadata and pass it to the --version.
   if lspci | grep -q -i NVIDIA; then
     local driver_version="latest"
-    echo "Pre-installing GPU driver ${driver_version}"
+    log "Pre-installing GPU driver ${driver_version}"
 
     # Note #1: --no-verify is to skip loading kernel modules. There is a bug of suspend/resume on GPU VMs if kernel modules are loaded.
     # During post-resume, the GPU device plugin will attempt to install the driver again.
     # The cos-gpu-installer will find the driver files are existed thus skipping downloading. And it will load kernel modules.
     cos-extensions install gpu -- --version="latest" --no-verify --host-dir /home/kubernetes/bin/nvidia
   else
-    echo "Nothing to be done"
+    log "No GPU detected; skipping GPU driver install"
   fi
 
-  # If there is any error above, the whole script will be aborted and then suspestion won't happen.
-  # GCE SuspendStates service will timeout and fail the process of generating suspended states.
-
-  echo "Marking as ready to be suspended..."
+  log "Marking as ready to be suspended in MDS..."
   write_to_mds "$INIT_STATE_GUEST_ATTRIBUTE" "INITIALIZED"
   wait_for_suspension
-  echo "Continuing the bootstrapping..."
+
+  log "Continuing the placeholder bootstrapping after resume..."
+
+  # b/365605093 - On resume, gve must be reloaded because mac address is changed on resume and must be refreshed
+  log "Reloading gvnic"
+  rmmod gve; modprobe gve
+
+  log "Wait for systemd-networkd-wait-online..."
+  systemctl restart systemd-networkd-wait-online.service
+
+  log "Wait for gcr connectivity..."
+  systemctl restart gcr-wait-online.service
+
+  log "Restart google-guest-agent.service..."
+  systemctl restart google-guest-agent.service
+
+  log "Done with placeholders-presuspend.sh"
 }
 
 handle_placeholder_vm
