a560f8e5d0bcae467a70429339249b2245a5a851
lucasmanning <lucasmanning@google.com>
2025-01-09 00:36:06 +0000

    Add support for GVISOR_REPRODUCE_NFTABLES
    This environment variable contains the desired value
    for the --reproduce-nftables flag in runsc.
    
    Change-Id: I349082b5c1a7f0473349ba309b91ddad8ed0c74e

--- gke/cluster/gce/gci/gke-internal-configure-helper.sh
+++ gke/cluster/gce/gci/gke-internal-configure-helper.sh
@@ -754,6 +754,7 @@ EOF
     local -r gvisor_net_raw="${GVISOR_NET_RAW:-"true"}"
     local -r gvisor_seccomp="${GVISOR_SECCOMP:-"true"}"
     local -r gvisor_core_tags="${GVISOR_CORE_TAGS:-"false"}"
+    local -r gvisor_reproduce_nftables="${GVISOR_REPRODUCE_NFTABLES:-"false"}"
     mkdir -p "${sandbox_root}"
     cat > "${shim_config_path}" <<EOF
 binary_name = "${CONTAINERD_SANDBOX_RUNTIME_ENGINE:-}"
@@ -764,6 +765,7 @@ root = "${sandbox_root}"
   oci-seccomp = "${gvisor_seccomp}"
   systemd-cgroup = "${systemdCgroup}"
   enable-core-tags = "${gvisor_core_tags}"
+  reproduce-nftables = "${gvisor_reproduce_nftables}"
 EOF
     if [[ -n "${GVISOR_METRIC_SERVER:-}" ]]; then
       echo "  metric-server = \"${GVISOR_METRIC_SERVER}\"" >> "${shim_config_path}"
