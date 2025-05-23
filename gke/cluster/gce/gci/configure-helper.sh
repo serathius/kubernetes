94ae0e9da3d331f26cade5565081204f73dd7d59
Marek Chodor <mchodor@google.com>
2025-05-23 12:37:27 +0000

    Add bridge traffic rules for gke workload identity with IPv6 stack type
    Change-Id: I5f38dca14e71a66bd6a0a28d8ed58b22a9c4677f
    Bug: 383096175

--- gke/cluster/gce/gci/configure-helper.sh
+++ gke/cluster/gce/gci/configure-helper.sh
@@ -27,6 +27,10 @@ set -o pipefail
 
 ### Hardcoded constants
 METADATA_SERVER_IP="${METADATA_SERVER_IP:-169.254.169.254}"
+# MDS IPv6 address is from the ULA range as selected by GCE MDS team in the
+# go/gce-ipv6-mds-design. They decided on ULA vs LL, as for LL in IPv6 one has
+# to explicitly specify the interface to use.
+METADATA_SERVER_IPV6="${METADATA_SERVER_IPV6:-fd20:ce::254}"
 
 # Backend endpoints (configurable for TPC).
 # May be overridden when kube-env is sourced.
@@ -228,16 +232,42 @@ function config-ip-firewall {
   # node because we don't expect the daemonset to run on this node.
   if [[ "${ENABLE_METADATA_CONCEALMENT:-}" == "true" ]] && [[ ! "${METADATA_CONCEALMENT_NO_FIREWALL:-}" == "true" ]]; then
     echo "Add rule for metadata concealment"
-    ip addr add dev lo 169.254.169.252/32 scope host
-    iptables -w -t nat -I PREROUTING -p tcp ! -i eth0 -d "${METADATA_SERVER_IP}" --dport 80 -m comment --comment "metadata-concealment: bridge traffic to metadata server goes to metadata proxy" -j DNAT --to-destination 169.254.169.252:988
-    iptables -w -t nat -I PREROUTING -p tcp ! -i eth0 -d "${METADATA_SERVER_IP}" --dport 8080 -m comment --comment "metadata-concealment: bridge traffic to metadata server goes to metadata proxy" -j DNAT --to-destination 169.254.169.252:987
-    if [[ "${ENABLE_S2A_PORT:-}" == "true" ]]; then
-      iptables -w -t nat -I PREROUTING -p tcp ! -i eth0 -d "${METADATA_SERVER_IP}" --dport 8082 -m comment --comment "metadata-concealment: bridge traffic to metadata server goes to metadata proxy" -j DNAT --to-destination 169.254.169.252:991
+    if [[ "${STACK_TYPE:-}" == "IPV6" ]]; then
+      # cluster stackType indicate it supports IPv6
+
+      # Address the gke-metadata-server listens to in IPv6 clusters
+      # It's an ULA address reserved in https://ipdb.corp.google.com/ which
+      # ensures no IPv6 conflict. Design go/mds-ipv6-for-gke.
+      local ipv6_gke_mds_listen_address="fd20:ce::252"
+      # Assigning address to lo interface with host scope to ensure it's not
+      # accessible from outside.
+      ip -6 addr add dev lo "${ipv6_gke_mds_listen_address}/128" scope host
+      ip6tables -w -t nat -I PREROUTING -p tcp ! -i eth0 -d "${METADATA_SERVER_IPV6}" --dport 80 -m comment --comment "metadata-concealment: bridge traffic to metadata server goes to metadata proxy" -j DNAT --to-destination "[${ipv6_gke_mds_listen_address}]:988"
+      ip6tables -w -t nat -I PREROUTING -p tcp ! -i eth0 -d "${METADATA_SERVER_IPV6}" --dport 8080 -m comment --comment "metadata-concealment: bridge traffic to metadata server goes to metadata proxy" -j DNAT --to-destination "[${ipv6_gke_mds_listen_address}]:987"
+      if [[ "${ENABLE_S2A_PORT:-}" == "true" ]]; then
+        ip6tables -w -t nat -I PREROUTING -p tcp ! -i eth0 -d "${METADATA_SERVER_IPV6}" --dport 8082 -m comment --comment "metadata-concealment: bridge traffic to metadata server goes to metadata proxy" -j DNAT --to-destination "[${ipv6_gke_mds_listen_address}]:991"
+      fi
+    fi
+    if [[ "${STACK_TYPE:-}" != "IPV6" ]]; then
+      # cluster stackType indicate it supports IPv4
+      ip addr add dev lo 169.254.169.252/32 scope host
+      iptables -w -t nat -I PREROUTING -p tcp ! -i eth0 -d "${METADATA_SERVER_IP}" --dport 80 -m comment --comment "metadata-concealment: bridge traffic to metadata server goes to metadata proxy" -j DNAT --to-destination 169.254.169.252:988
+      iptables -w -t nat -I PREROUTING -p tcp ! -i eth0 -d "${METADATA_SERVER_IP}" --dport 8080 -m comment --comment "metadata-concealment: bridge traffic to metadata server goes to metadata proxy" -j DNAT --to-destination 169.254.169.252:987
+      if [[ "${ENABLE_S2A_PORT:-}" == "true" ]]; then
+        iptables -w -t nat -I PREROUTING -p tcp ! -i eth0 -d "${METADATA_SERVER_IP}" --dport 8082 -m comment --comment "metadata-concealment: bridge traffic to metadata server goes to metadata proxy" -j DNAT --to-destination 169.254.169.252:991
+      fi
     fi
   fi
-  iptables -w -t mangle -I OUTPUT -s "${METADATA_SERVER_IP}" -j DROP
-  iptables -w -t mangle -I OUTPUT -s "${METADATA_SERVER_IP}" -p udp --sport 53 -j ACCEPT
-  iptables -w -t mangle -I OUTPUT -s "${METADATA_SERVER_IP}" -p tcp --sport 53 -j ACCEPT
+  if [[ "${STACK_TYPE:-}" != "IPV6" ]]; then
+    iptables -w -t mangle -I OUTPUT -s "${METADATA_SERVER_IP}" -j DROP
+    iptables -w -t mangle -I OUTPUT -s "${METADATA_SERVER_IP}" -p udp --sport 53 -j ACCEPT
+    iptables -w -t mangle -I OUTPUT -s "${METADATA_SERVER_IP}" -p tcp --sport 53 -j ACCEPT
+  fi
+  if [[ "${STACK_TYPE:-}" == "IPV6" ]]; then
+    ip6tables -w -t mangle -I OUTPUT -s "${METADATA_SERVER_IPV6}" -j DROP
+    ip6tables -w -t mangle -I OUTPUT -s "${METADATA_SERVER_IPV6}" -p udp --sport 53 -j ACCEPT
+    ip6tables -w -t mangle -I OUTPUT -s "${METADATA_SERVER_IPV6}" -p tcp --sport 53 -j ACCEPT
+  fi
 
   # Log all metadata access not from approved processes.
   case "${METADATA_SERVER_FIREWALL_MODE:-off}" in
