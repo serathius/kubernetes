272d19c8842194b8ce02b34c01824eb67a9b54cd
Marek Siarkowicz <siarkowicz@google.com>
2025-10-22 16:22:40 +0200

    Benchmark SharedIndexInformer

--- staging/src/k8s.io/client-go/tools/cache/the_real_fifo.go
+++ staging/src/k8s.io/client-go/tools/cache/the_real_fifo.go
@@ -43,6 +43,7 @@ type RealFIFOOptions struct {
 	// If set, will be called for objects before enqueueing them. Please
 	// see the comment on TransformFunc for details.
 	Transformer TransformFunc
+	BatchSize   int
 }
 
 const (
@@ -500,13 +501,16 @@ func NewRealFIFOWithOptions(opts RealFIFOOptions) *RealFIFO {
 	if opts.KnownObjects == nil {
 		panic("coding error: knownObjects must be provided")
 	}
+	if opts.BatchSize == 0 {
+		opts.BatchSize = defaultBatchSize
+	}
 
 	f := &RealFIFO{
 		items:        make([]Delta, 0, 10),
 		keyFunc:      opts.KeyFunction,
 		knownObjects: opts.KnownObjects,
 		transformer:  opts.Transformer,
-		batchSize:    defaultBatchSize,
+		batchSize:    opts.BatchSize,
 	}
 
 	f.cond.L = &f.lock
