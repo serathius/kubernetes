dc4855dbb314733f8fd5ee84eb09167e4321ea1e
Marek Siarkowicz <siarkowicz@google.com>
2026-04-27 10:22:39 +0200

    apiserver/responsewriters: Update BenchmarkSerializeObject to use exemplar pod
    This change modifies BenchmarkSerializeObject to use the proposed
    exemplar pod shape instead of the default pod.json. It also updates
    benchmarkItems to support loading YAML files.
    
    Results (geomean):
    - sec/op: +246.96%
    - writtenBytes/op: +241.01%
    
    Detailed benchstat comparison shows ~210% to +360% increase in time
    and ~180% to +280% increase in bytes written across different counts
    and media types.

--- staging/src/k8s.io/apiserver/pkg/endpoints/handlers/responsewriters/writers_test.go
+++ staging/src/k8s.io/apiserver/pkg/endpoints/handlers/responsewriters/writers_test.go
@@ -31,6 +31,8 @@ import (
 	"net/url"
 	"os"
 	"reflect"
+	"strings"
+	"sigs.k8s.io/yaml"
 	"strconv"
 	"testing"
 	"time"
@@ -671,12 +673,15 @@ func randPod(b *testing.B, pod *v1.Pod, r *rand.Rand) {
 
 func benchmarkItems(b *testing.B, file string, n int) *v1.PodList {
 	pod := v1.Pod{}
-	f, err := os.Open(file)
+	data, err := os.ReadFile(file)
 	if err != nil {
-		b.Fatalf("Failed to open %q: %v", file, err)
+		b.Fatalf("Failed to read %q: %v", file, err)
 	}
-	defer f.Close()
-	err = json.NewDecoder(f).Decode(&pod)
+	if strings.HasSuffix(file, ".yaml") {
+		err = yaml.Unmarshal(data, &pod)
+	} else {
+		err = json.Unmarshal(data, &pod)
+	}
 	if err != nil {
 		b.Fatalf("Failed to decode %q: %v", file, err)
 	}
@@ -749,7 +754,7 @@ func BenchmarkSerializeObject(b *testing.B) {
 				{"Json", toJSON},
 				{"Protobuf", toProtoBuf},
 			}
-			podList := benchmarkItems(b, "testdata/pod.json", count)
+			podList := benchmarkItems(b, "testdata/exemplar_pod.yaml", count)
 			for _, media := range medias {
 				b.Run(fmt.Sprintf("MediaType=%s", media.name), func(b *testing.B) {
 					payload := media.convert(b, podList)
