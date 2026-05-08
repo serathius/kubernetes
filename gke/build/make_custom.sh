2ad4634706cf7400476019906b14150f4892876c
Benjamin Elder <bentheelder@google.com>
2026-05-08 16:04:09 -0700

    fix gke-internal copyright boilerplate
    Change-Id: Iafa25ba4110151d9ae432707a8dd329cb58348e0

--- gke/build/make_custom.sh
+++ gke/build/make_custom.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
+# Copyright 2020 Google
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Build GKE. This is just a thin wrapper around lib.sh. Among the other
 # ./make_*.sh scripts, this is the only one that assumes nothing about the
 # build. That is, this script is the entrypoint where you have the most control.
