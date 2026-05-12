9917f60299f422d27eabc8a3f1429a5dd0c18425
Benjamin Elder <bentheelder@google.com>
2026-05-11 17:37:11 -0700

    set -e in verify-boilerplate so failures exit non-zero ...
    BUG=511376935
    
    Change-Id: Ibedb88e0153bc3ae721ed1547cc3cb8a7ae88db7

--- gke/hack/verify-boilerplate.sh
+++ gke/hack/verify-boilerplate.sh
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -uo pipefail
+set -euo pipefail
 
 REPO_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)"
 
