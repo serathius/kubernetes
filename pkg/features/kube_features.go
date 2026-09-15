c048566efdc55e5e4d6d2a33779bf02786104d05
Marek Siarkowicz <siarkowicz@google.com>
2026-09-15 09:26:26 +0200

    Enable WatchCacheStallResume by default

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -2421,7 +2421,7 @@ var defaultVersionedKubernetesFeatureGates = map[featuregate.Feature]featuregate
 	},
 
 	genericfeatures.WatchCacheStallResume: {
-		{Version: version.MustParse("1.38"), Default: false, PreRelease: featuregate.Alpha},
+		{Version: version.MustParse("1.38"), Default: true, PreRelease: featuregate.Beta},
 	},
 
 	genericfeatures.WatchList: {
